4324c3b17eb650b4c26eb4783c8a0b9d422470c7
miguelgc95 <miguelgc95@gmail.com>
2021-02-01 00:13:20 +0100

    playing with types

---
+++ my-library/types.php
@@ -0,0 +1,38 @@
+<?php
+
+$myBool = true;
+$myInt = 1;
+$myFloat = 0.1;
+$myString = "cad";
+$myArr = [1,2,3];
+$myNull = null;
+
+class car{
+    public $prop = 12;
+}
+
+$myObj2 = new car;
+
+var_dump($myBool);
+echo "<br>";
+echo "<br>";
+
+var_dump($myInt);
+echo "<br>";
+echo "<br>";
+var_dump($myFloat);
+echo "<br>";
+echo "<br>";
+var_dump($myString);
+echo "<br>";
+echo "<br>";
+var_dump($myArr);
+echo "<br>";
+echo "<br>";
+var_dump($myObj2);
+
+echo "<br>";
+echo "<br>";
+var_dump($myNull);
+
+?>
